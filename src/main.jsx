2ed954043ad64f67120573bab5d43e2ce4ece40a
Daniela Moreno <dani9292@hotmail.com>
2023-08-21 11:28:30 -0500

    set up store.js

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import store from './routes/redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
     <App />
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 )
