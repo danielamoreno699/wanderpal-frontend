82a124f861fc61dee3d03569a1146faa797f0693
Daniela Moreno <dani9292@hotmail.com>
2023-08-21 12:57:41 -0500

    create a card structure

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,28 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 
 const Home = () => {
   return (
-      <h2>Here goes main page</h2>
+    <div className='cards'>
+      <h2> here displays cards</h2>
+      <div className='card-cont'>
+
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the cards content.
+        </Card.Text>
+        <Button variant="primary">Go to details</Button>
+        <Button variant="primary">make a reservation</Button>
+      </Card.Body>
+    </Card>
+    </div>
+    </div>
+    
   );
 };
 
