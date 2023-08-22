aed7accabb61096e7c5cf0adabbbc4f55b6dc020
julzedz <edoziechukwuma@gmail.com>
2023-08-22 17:07:38 +0100

    Add onHandleSelect function to delete button

--- src/components/reservation.jsx
+++ src/components/reservation.jsx
@@ -2,11 +2,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { useNavigate } from "react-router-dom";
 // import "../styles/Home.css";
-import { fetchReservations } from "../redux/reservationsSlice";
+import { fetchReservations, selectedReservation } from "../redux/reservationsSlice";
 
 const Reservation = () => {
   const dispatch = useDispatch();
+    const navigate = useNavigate();
   const { reservations, status } = useSelector((state) => state.reservations);
 
   useEffect(() => {
@@ -15,18 +17,25 @@ const Reservation = () => {
     }
   }, [status, dispatch, reservations]);
 
+  const onHandleSelect = (id) => {
+    dispatch(selectedReservation(id));
+    navigate(`/Details/${id}`);
+  };
+
   return (
     <div className="cards">
-      <h2>Here displays cards</h2>
+      <h2>Reservations</h2>
       <div className="card-cont">
         {reservations.map((reservation, index) => (
           <Card key={index} style={{ width: "18rem" }}>
-            <Card.Img variant="top" src='' alt='image' />
+            <Card.Img variant="top" src="" alt="image" />
             <Card.Body>
               <Card.Title>{reservation.city}</Card.Title>
               <Card.Text>{reservation.date}</Card.Text>
               <Card.Text>Here goes the name of the item</Card.Text>
-              <Button variant="primary">Delete</Button>
+              <Button variant="primary" onClick={() => onHandleSelect(reservation.id)}>
+                Delete
+              </Button>
             </Card.Body>
           </Card>
         ))}
