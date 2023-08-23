663be2296b9fb42019ab7d4f72b1be3d3a0b0ea5
Daniela Moreno <dani9292@hotmail.com>
2023-08-23 15:34:25 -0500

    insert prop validations

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'; 
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
@@ -5,7 +7,30 @@ import Modal from 'react-bootstrap/Modal';
  const ModalReservation = ({ show, setShow, itemName }) => {
     const handleClose = () => setShow(false);
 
+
+
+    const [reservation, setReservation] = useState({
+        user_id: '',
+        date: '',
+        city: '',
+    })
+
+
     console.log('itemNameModal', itemName)
+
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        console.log(name, value)
+        setReservation({ ...reservation, [name]: value });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('reservation', reservation)
+        handleClose();
+    }
 
   return (
     <>
@@ -16,12 +41,15 @@ import Modal from 'react-bootstrap/Modal';
           <Modal.Title>Reservation for {itemName.name}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form  onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Select a date</Form.Label>
               <Form.Control
                 type="date"
                 autoFocus
+                onChange={handleChange}
+                value={reservation.date}
+                required
               />
             </Form.Group>
             <Form.Group
@@ -32,6 +60,9 @@ import Modal from 'react-bootstrap/Modal';
               <Form.Control
               type='text'
                 placeholder='City'
+                onChange={handleChange}
+                value={reservation.city}
+                required
               />
             </Form.Group>
           </Form>
@@ -49,4 +80,14 @@ import Modal from 'react-bootstrap/Modal';
   )
 }
 
+ModalReservation.propTypes = {
+  show: PropTypes.bool.isRequired,
+  setShow: PropTypes.func.isRequired,
+  itemName: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    
+  }).isRequired,
+};
+
 export default ModalReservation
+
