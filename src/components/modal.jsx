befb50c0dd07082c05ae0e79cff71e86c0e98803
Daniela Moreno <dani9292@hotmail.com>
2023-08-23 15:52:24 -0500

    fix inputs

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,95 +1,77 @@
-import PropTypes from 'prop-types'; 
-import { useState } from 'react';
+import React, { useState } from 'react'; // Don't forget to import React
+import PropTypes from 'prop-types'; // Import PropTypes
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 
- const ModalReservation = ({ show, setShow, itemName, submitReservation }) => {
-    const handleClose = () => setShow(false);
+const ModalReservation = ({ show, setShow, itemName, submitReservation }) => {
+  const handleClose = () => setShow(false);
 
+  const [reservation, setReservation] = useState({
+    user_id: '',
+    date: '',
+    city: '',
+  });
 
+  const onHandleChange = (e) => {
+    const { name, value } = e.target;
+    setReservation((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-    const [reservation, setReservation] = useState({
-        user_id: '',
-        date: '',
-        city: '',
-    })
-
-
-    console.log('itemNameModal', itemName)
-
-
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        console.log(name, value)
-        setReservation({ ...reservation, [name]: value });
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('reservation', reservation)
-        submitReservation(reservation);
-        handleClose();
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    submitReservation(reservation);
+    handleClose();
+  };
 
   return (
     <>
-     
-
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Reservation for {itemName.name}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form  onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Select a date</Form.Label>
-              <Form.Control
-                type="date"
-                autoFocus
-                onChange={handleChange}
-                value={reservation.date}
-                required
-              />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Indicate your city</Form.Label>
-              <Form.Control
-              type='text'
-                placeholder='City'
-                onChange={handleChange}
-                value={reservation.city}
-                required
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Submit
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  )
-}
+      <Modal.Header closeButton>
 
-ModalReservation.propTypes = {
-  show: PropTypes.bool.isRequired,
-  setShow: PropTypes.func.isRequired,
-  submitReservation: PropTypes.func.isRequired,
-  itemName: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    
-  }).isRequired,
+        <Modal.Title>Reservation for {itemName.name}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form >
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Select a date</Form.Label>
+            <Form.Control
+              type="date"
+              name="date"
+              value={reservation.date}
+              onChange={onHandleChange}
+              autoFocus
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Indicate your city</Form.Label>
+            <Form.Control
+              type="text"
+              name="city"
+              placeholder="City"
+              value={reservation.city}
+              onChange={onHandleChange}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" >
+          Submit
+        </Button>
+      </Modal.Footer>
+    </Modal>
+    </>
+  );
 };
 
-export default ModalReservation
+// Prop validations
 
+
+export default ModalReservation;
