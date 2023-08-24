176a9058860635f35b027ec6788f2cd292449702
julzedz <edoziechukwuma@gmail.com>
2023-08-24 12:58:04 +0100

    Update reservation form component

--- src/components/reservation-form.jsx
+++ src/components/reservation-form.jsx
@@ -1,15 +1,31 @@
 import Form from "react-bootstrap/Form";
 import { FloatingLabel, Button } from "react-bootstrap";
 import "../styles/reservation.css";
+import { useDispatch } from "react-redux";
+import { createReservation } from "../redux/ReservationCreateSlice";
 
 const ReservationForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Get the form data
+    const formData = {
+      name: "John Doe", // Example name
+      city: "New York", // Example city
+      date: "2023-08-25", // Example date
+    };
+    // Dispatch the createReservation action
+    dispatch(createReservation(formData));
+  };
   return (
     <>
       <div className="cards-home">
         <h2 className="home-h2">Reservation Form</h2>
         <hr className="hr-home" />
         <br />
-        <Form className="form-width">
+        <Form className="form-width" onSubmit={handleSubmit}>
           <Form.Select aria-label="Default select example" className="mb-3">
             <option>Pick a Tour</option>
             <option value="1">Tour 1</option>
