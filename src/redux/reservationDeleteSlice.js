9e3b3e3ed43359562ce615cc37326f4d42a387c9
Daniela Moreno <dani9292@hotmail.com>
2023-08-25 10:30:20 -0500

    return reservation id y itemid

--- src/redux/reservationDeleteSlice.js
+++ src/redux/reservationDeleteSlice.js
@@ -1,60 +1,60 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-
 import axios from 'axios';
 
 const initialState = {
     status: "idle",
     items:[],
-    loading: false
+    loading: false,
+    deletedItem: {}
 }
 
 export const deleteReservationApi = createAsyncThunk(
     "reservationDeleteItem/deleteReservationItem",
-    async ( {reservationId, itemId} ) => {
-      console.log("Deleting reservation with reservationId:", reservationId);
-      console.log("Deleting item with itemId:", itemId);
-      
-      try {
-        const response = await axios.delete(`http://127.0.0.1:3001/api/v1/items/${itemId}/reservations/${reservationId}`);
-        console.log("Delete response status:", response.status);
-        console.log("Delete response headers:", response.headers);
-        console.log("Delete response data:", response.data);
-        return response.data;
-      } catch (error) {
-        console.error("Delete error:", error);
-        throw error;
-      }
-    }
-  );
-  
+    async ({ reservationId, itemId }) => {
+        console.log("Deleting reservation with reservationId:", reservationId);
+        console.log("Deleting item with itemId:", itemId);
 
-    const deleteReservationItemSlice = createSlice({
-        name: "reservationDeleteItem",
-        initialState,
-        reducers: {
-            resetDeletedItems: (state) => {
-              state.items = [];
-            }
-          },
-        
-        extraReducers: (builder) => {
-            builder
-              .addCase(deleteReservationApi.pending, (state) => {
+        try {
+            const response = await axios.delete(`http://127.0.0.1:3001/api/v1/items/${itemId}/reservations/${reservationId}`);
+            console.log("Delete response status:", response.status);
+            console.log("Delete response headers:", response.headers);
+            console.log("Delete response data:", response.data);
+            return {
+                reservationId, itemId
+            };
+        } catch (error) {
+            console.error("Delete error:", error);
+            throw error;
+        }
+    }
+);
+
+const deleteReservationItemSlice = createSlice({
+    name: "reservationDeleteItem",
+    initialState,
+    reducers: {
+        resetDeletedItems: (state) => {
+            state.items = [];
+        }
+    },
+
+    extraReducers: (builder) => {
+        builder
+            .addCase(deleteReservationApi.pending, (state) => {
                 state.loading = true;
-              })
-              .addCase(deleteReservationApi.fulfilled, (state, action) => {
+            })
+            .addCase(deleteReservationApi.fulfilled, (state, action) => {
                 state.loading = false;
-                const deletedItemId = action.payload;
-                console.log('Deleted item ID from slice:', deletedItemId);
-                 state.deletedItems = state.items.filter(itemId => itemId !== deletedItemId);
+                const { itemId, reservationId } = action.payload;
+                console.log('Deleted reservation ID:', reservationId);
+                console.log('Deleted item ID:', itemId);
                
-              })
-           
-              .addCase(deleteReservationApi.rejected, (state) => {
+            })
+            .addCase(deleteReservationApi.rejected, (state) => {
                 state.loading = false;
-              });
-          },
-    })
+            });
+    },
+});
 
 export const { resetDeletedItems } = deleteReservationItemSlice.actions;
 export default deleteReservationItemSlice.reducer;
