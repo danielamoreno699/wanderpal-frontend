666e44b64f436899b0c2169ea8ee7af9e0f3115d
julzedz <edoziechukwuma@gmail.com>
2023-08-26 01:41:16 +0100

    Set up itemId and userId

--- src/components/reservation-form.jsx
+++ src/components/reservation-form.jsx
@@ -3,40 +3,40 @@ import Form from "react-bootstrap/Form";
 import { FloatingLabel, Button } from "react-bootstrap";
 import "../styles/reservation.css";
 import { useDispatch, useSelector } from "react-redux";
-import { createReservation, fetchTourOptions } from "../redux/ReservationCreateSlice";
+import { createReservation } from "../redux/ReservationCreateSlice";
+import { fetchItems } from "../redux/itemsSlice";
 
 const ReservationForm = () => {
   const dispatch = useDispatch();
-  const { reservations } = useSelector((state) => state.reservations);
-  const { items } = useSelector((state) => state.items);
-  const [selectedTour, setSelectedTour] = useState("");
-  const userId = reservations.map((reservation) => reservation.user_id);
 
-  // Fetch tour options when the component mounts
+  const { items } = useSelector((state) => state.items);
+
+  const [selectedTour, setSelectedTour] = useState({
+    itemId: "",
+    user_id: "",
+    date: "",
+    city: "",
+  });
+
+  const onHandleChange = (e) => {
+    const { name, value } = e.target;
+    const parsedValue = name === "itemId" ? parseInt(value, 10) : value;
+    const selectedUserId = items.find((item) => item.user_id);
+    setSelectedTour((prev) => ({
+      ...prev,
+      [name]: parsedValue,
+      user_id: selectedUserId.user_id,
+    }));
+  };
+
   useEffect(() => {
-    dispatch(fetchTourOptions());
+    dispatch(fetchItems());
   }, [dispatch]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    // Get the form data
-    const formData = {
-      user_id: userId,
-      name: selectedTour,
-      city: e.target.elements.city.value,
-      date: e.target.elements.date.value,
-    };
-    // Dispatch the createReservation action
-    dispatch(createReservation({ formData, userId }));
-    console.log(formData);
-
-    // Clear the selected tour
-    setSelectedTour("");
-  };
-
-  const handleTourChange = (e) => {
-    setSelectedTour(e.target.value);
+    dispatch(createReservation(selectedTour));
+    console.log(selectedTour);
   };
 
   return (
@@ -49,24 +49,47 @@ const ReservationForm = () => {
           <Form.Select
             aria-label="Select a Tour"
             className="mb-3"
-            onChange={handleTourChange}
-            value={selectedTour}
+            onChange={onHandleChange}
+            value={selectedTour.itemId}
+            name="itemId"
           >
             <option>Pick a Tour</option>
-            {items.map((item) => (
-              <option key={item.id} value={item.name}>
-                {item.name}
-              </option>
-            ))}
+
+            {items.map(
+              (item) => (
+                console.log(item.name),
+                console.log("this is the user id", item.user_id),
+                (
+                  <option key={item.id} value={item.id}>
+                    {item.name}
+                  </option>
+                )
+              )
+            )}
           </Form.Select>
+
+          <FloatingLabel label="Date" className="mb-3">
+            <Form.Control
+              type="Date"
+              name="date"
+              placeholder="Date"
+              value={selectedTour.date}
+              onChange={onHandleChange}
+            />
+          </FloatingLabel>
+
           <br />
           <FloatingLabel label="City" className="mb-3">
-            <Form.Control type="text" name="city" placeholder="City" />
+            <Form.Control
+              type="text"
+              name="city"
+              placeholder="City"
+              onChange={onHandleChange}
+              value={selectedTour.city}
+            />
           </FloatingLabel>
           <br />
-          <FloatingLabel label="Date" className="mb-3">
-            <Form.Control type="Date" name="date" placeholder="Date" />
-          </FloatingLabel>
+
           <br />
 
           <Button variant="primary" type="submit">
--- src/redux/ReservationCreateSlice.js
+++ src/redux/ReservationCreateSlice.js
@@ -1,82 +1,48 @@
-// ReservationCreateSlice.js
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from 'axios';
+import axios from "axios";
 
-// Define the initial state
 const initialState = {
-  status: "idle", // "idle", "loading", "succeeded", "failed"
+  status: "idle",
   error: null,
-  reservation: null,
-  tourOptions: [], // New state to store tour options
+
+  tourOptions: [],
 };
 
-// Define an async thunk to fetch tour options
-export const fetchTourOptions = createAsyncThunk(
-  "reservationCreate/fetchTourOptions",
-  async (_, { rejectWithValue }) => {
-    try {
-      const response = await fetch("http://[::1]:3001/api/v1/items");
-
-      if (!response.ok) {
-        throw new Error("Failed to fetch tour options");
-      }
-
-      const data = await response.json();
-      return data.map((item) => ({
-        value: item.id.toString(),
-        label: item.name,
-      }));
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
-  }
-);
-
-// Define the async thunk to create a reservation
 export const createReservation = createAsyncThunk(
   "reservationCreateItem/createReservationItem",
 
-  async ({ name, userId, date, city }) => {
+  async ({ itemId, user_id, date, city }) => {
     const payload = {
-      user_id: userId,
+      itemId,
+
+      user_id,
       date,
       city,
     };
     console.log(payload);
+
     const response = await axios.post(
-      `http://127.0.0.1:3001/api/v1/items/${name}/reservations`,
+      `http://127.0.0.1:3001/api/v1/items/${itemId}/reservations`,
       payload
     );
     return response.data;
   }
 );
 
-// Create a slice
 const reservationCreateSlice = createSlice({
   name: "reservationCreate",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchTourOptions.pending, (state) => {
-        state.status = "loading";
-        state.error = null;
-      })
-      .addCase(fetchTourOptions.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        state.tourOptions = action.payload; // Store tour options in state
-      })
-      .addCase(fetchTourOptions.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.payload;
-      })
+
       .addCase(createReservation.pending, (state) => {
         state.status = "loading";
         state.error = null;
       })
       .addCase(createReservation.fulfilled, (state, action) => {
         state.status = "succeeded";
-        state.reservation = action.payload;
+        state.tourOptions = action.payload;
       })
       .addCase(createReservation.rejected, (state, action) => {
         state.status = "failed";
@@ -85,7 +51,6 @@ const reservationCreateSlice = createSlice({
   },
 });
 
-// Define the selectTourOptions selector
 export const selectTourOptions = (state) => state.reservationCreate.tourOptions;
 
 export default reservationCreateSlice.reducer;
