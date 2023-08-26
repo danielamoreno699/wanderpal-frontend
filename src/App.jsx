9ab7fb0e4ac2a613463ae0b0fa895ad0ba8feeaf
Daniela Moreno <dani9292@hotmail.com>
2023-08-26 11:46:57 -0500

    protect private routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import Layout from './components/Layout';
 import RegistrationForm from './components/registration-form';
 import Login from './components/login';
 import WanderPalRoutes from './routes/WanderPalRoutes';
+import RequiredAuth from './components/RequiredAuth';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+
         {/*public routes */}
         <Route path="login" element={<Login />} />
         <Route path="registration" element={<RegistrationForm />} />
 
         {/*private routes */}
+        <Route element={<RequiredAuth />}> 
         <WanderPalRoutes /> 
+        </Route>
 
         
 
