5573992fe5384dddf7914ae9d4b1678fd4c5717b
Daniela Moreno <dani9292@hotmail.com>
2023-08-27 10:27:18 -0500

    update login component

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,27 +1,24 @@
 import  { useRef, useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { useDispatch } from 'react-redux'; 
-import useAuth from '../hooks/useAuth';
+import { useDispatch, useSelector } from 'react-redux';
 import { userAuthApi } from '../redux/createUsersSlice';
-import { useSelector } from 'react-redux';
-
+import useAuth from '../hooks/useAuth';
 
 const Login = () => {
-
-    const { loggingIn } = useSelector((state) => state.getUsers);
-
-
     const { setAuth } = useAuth();
     const location = useLocation();
     const home = location.state?.from || { pathname: '/' };
 
     const navigate = useNavigate();
     const userRef = useRef();
+    const pwdRef = useRef();
     const errRef = useRef();
 
     const [user, setUser] = useState('');
     const [pwd, setPwd] = useState('');
     const [errMsg, setErrMsg] = useState('');
+
+    const { loggingIn, loggingOut } = useSelector((state) => state.getUsers);
 
     useEffect(() => {
         userRef.current.focus();
@@ -31,7 +28,7 @@ const Login = () => {
         setErrMsg('');
     }, [user, pwd]);
 
-    const dispatch = useDispatch(); 
+    const dispatch = useDispatch();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -39,16 +36,17 @@ const Login = () => {
         if (user && pwd) {
             try {
                 dispatch(loggingIn());
+
                 const response = await dispatch(userAuthApi({ user, password: pwd }));
                 console.log('response:', response);
                 if (response) {
                     setAuth(true);
-                    loggingIn
                     navigate(home, { replace: true });
                 }
             } catch (error) {
                 setErrMsg('Invalid username or password');
-                
+            } finally {
+                dispatch(loggingOut());
             }
         } else {
             setErrMsg('Please enter both username and password');
@@ -79,12 +77,12 @@ const Login = () => {
                     <input
                         type="password"
                         id="password"
-                        ref={pwd} 
+                        ref={pwdRef}
                         onChange={(e) => setPwd(e.target.value)}
                         value={pwd}
                         required
                     />
-                    <button>Sign in</button>
+                    <button disabled={loggingIn}>Sign in</button>
                 </form>
 
                 <p>
