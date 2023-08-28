2d155e3400226f748e34f8e438338ec2fcba530d
Daniela Moreno <dani9292@hotmail.com>
2023-08-28 13:24:05 -0500

    make changes

--- src/components/RequiredAuth.jsx
+++ src/components/RequiredAuth.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 import axios from 'axios';
@@ -6,31 +6,31 @@ import axios from 'axios';
 const RequiredAuth = () => {
   const { auth } = useAuth();
   const [usernames, setUsernames] = useState([]);
-
-  const fetchUsernames = async () => {
-    try {
-      const response = await axios.get('http://127.0.0.1:3001/api/v1/users');
-      setUsernames(response.payload.map(user => user.name));
-    } catch (error) {
-      console.error('Error fetching usernames:', error);
-    }
-  };
+  const [setLoading] = useState(true);
 
   useEffect(() => {
+    const fetchUsernames = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:3001/api/v1/users');
+        setUsernames(response.data.map(user => user.name));
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching usernames:', error);
+        setLoading(false);
+      }
+    };
+
     fetchUsernames();
   }, []);
 
-  const checkUsernameExistence = (username) => {
-    return usernames.includes(username);
-  };
+  
 
   if (auth && auth.username) {
-    const usernameExists = checkUsernameExistence(auth.username);
+    const usernameExists = usernames.includes(auth.username);
 
     if (usernameExists) {
       return <Outlet />;
     } else {
-     
       return <Navigate to="/login" replace />;
     }
   } else {
