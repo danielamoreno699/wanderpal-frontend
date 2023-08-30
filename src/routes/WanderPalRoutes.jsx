396d9fcf574b94d5b8215c38c44fb2d91f99f88d
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-08-30 07:50:55 -0400

    Make navbar responsive

--- src/routes/WanderPalRoutes.jsx
+++ src/routes/WanderPalRoutes.jsx
@@ -1,5 +1,4 @@
 import { Navigate, Route, Routes} from "react-router-dom";
-
 import Reservation from "../components/reservation";
 import CustomNavbar from "../components/navbar";
 import Items from "../components/items";
@@ -7,31 +6,20 @@ import ReservationForm from "../components/reservation-form";
 import Home from "../components/home";
 import ReserveItem from "../components/reservation-item-form";
 
-
-
-
 const WanderPalRoutes = () => (
-
     
     <div className="app-container">
         <CustomNavbar/>
         <div className="content-container">
             <Routes>
-
                 <Route path="Home" element={<Home/>}/>
                 <Route path="items/:id" element={<Items/>}/>
                 <Route path="/" element={<Navigate to="/Home" />} />
-
                 <Route path="/reservation" element={<Reservation/>}/>
-            
-
                 <Route path="/reservationform" element={<ReservationForm />}/>
                 <Route path = "/reservationItemForm/:id" element={<ReserveItem/>} />
             </Routes>
         </div>
-
-
-        
     </div >
 );
 
