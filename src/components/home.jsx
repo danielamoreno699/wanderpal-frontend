ecdfb5e1cfe9911aec71c018751ce6f65cda4581
Daniela Moreno <dani9292@hotmail.com>
2023-08-30 00:06:31 -0500

    made some changes to the home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useState } from 'react';
+
 import { useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 import { useNavigate } from 'react-router-dom';
 import { fetchItems, selectedItem } from '../redux/itemsSlice';
-import ModalReservation from './modal';
-import { createReservationApi } from '../redux/reservationCreateItemSlice';
-import Swal from 'sweetalert2';
+
 import { useAuth } from '../context/AuthProvider';
+
+import CarouselSlide from './caroursel';
+import Banner from './banner';
 
 // import CarouselSlide from './caroursel';
 // import Banner from './banner';
@@ -24,19 +25,13 @@ import 'swiper/css/scrollbar';
 import '../styles/Home.css';
 
 const Home = () => {
-
   const { isLoggedIn } = useAuth();
-  const [showModal, setShowModal] = useState(false);
-  const [selectedItemName, setSelectedItemName] = useState('');
-  // eslint-disable-next-line no-unused-vars
-  const [reservationSuccess, setReservationSuccess] = useState(false);
-
+ 
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const { items, status } = useSelector((state) => state.items);
- 
 
   useEffect(() => {
     if (status === 'idle' && items.length === 0) {
@@ -44,118 +39,75 @@ const Home = () => {
     }
   }, [status, dispatch, items]);
 
- 
-
   const onHandleSelect = (id) => {
     dispatch(selectedItem(id));
-    navigate(`/items/${id}`)
+    navigate(`/items/${id}`);
   }
 
-  const onHandleReserve = (item) => {
-    setSelectedItemName(item);
-    setShowModal(true);
-  }
 
-  const handleSubmitReservation =  (reservation) => {
-    const itemId = selectedItemName.id;
-    const { date, city } = reservation;
-    const userId = localStorage.getItem('user_id');
-   
+
   
-
-      const response =  dispatch(createReservationApi({ itemId, userId, date, city }));
-      
-  
-      if (response) {
-        setReservationSuccess(true);
-        Swal.fire({
-          icon: 'success',
-          title: 'Reservation Successful!',
-          text: 'Your reservation has been created successfully.',
-        });
-      }else{
-        Swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: 'Something went wrong!',
-        });
-      }
-
-  };
-
   return (
     <>
-    <div className='flex-basis'>
-     
-
-    <div className='cards-home'>
-      {/* {!isLoggedIn && <CarouselSlide />}
-      {!isLoggedIn && <Banner />} */}
-      <h2 className='home-h2'>Our Tours</h2>
-      <hr className='hr-home' />
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={20}
-        slidesPerView={4}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-        breakpoints={{
-          0: {
-            slidesPerView: 1,
-          },
-          768: {
-            slidesPerView: 2,
-          },
-          992: {
-            slidesPerView: 4,
-          },
-        }}
-      >
-        {items.map((item, index) => (
-          <SwiperSlide key={index} className='swiper-slide'>
-            <Card className='card-item'>
-              <Card.Img variant='top' src={item.image} className='card-img-item' alt='img' />
-              <Card.Body>
-                <Card.Title className='card-title-item'>{item.name}</Card.Title>
-                <Card.Text className='card-item-price'>{item.price}$</Card.Text>
-                <div className='card-btns'>
-                  <Button
-                    variant='primary'
-                    onClick={() => onHandleSelect(item.id)}
-                    disabled={!isLoggedIn}
-                  >
-                    Go to details
-                  </Button>
-                  <Button
-                    variant='primary'
-                    onClick={() => onHandleReserve(item)}
-                    disabled={!isLoggedIn}
-                  >
-                    Make a reservation
-                  </Button>
-                </div>
-              </Card.Body>
-            </Card>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
-    </div>
-
-    <ModalReservation 
-    show={showModal} 
-    setShow={setShowModal} 
-    itemName={selectedItemName} 
-    submitReservation={handleSubmitReservation}
-    />
-    </div>
+      <div className='flex-basis'>
+        {isLoggedIn ? (
+          <div className='cards-home'>
+            <h2 className='home-h2'>Our Tours</h2>
+            <hr className='hr-home' />
+            <Swiper
+              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              spaceBetween={20}
+              slidesPerView={4}
+              navigation
+              pagination={{ clickable: true }}
+              scrollbar={{ draggable: true }}
+              onSwiper={(swiper) => console.log(swiper)}
+              onSlideChange={() => console.log('slide change')}
+              breakpoints={{
+                0: {
+                  slidesPerView: 1,
+                },
+                768: {
+                  slidesPerView: 2,
+                },
+                992: {
+                  slidesPerView: 4,
+                },
+              }}
+            >
+              {items.map((item, index) => (
+                <SwiperSlide key={index} className='swiper-slide'>
+                  <Card className='card-item'>
+                    <Card.Img variant='top' src={item.image} className='card-img-item' alt='img' />
+                    <Card.Body>
+                      <Card.Title className='card-title-item'>{item.name}</Card.Title>
+                      <Card.Text className='card-item-price'>{item.price}$</Card.Text>
+                      <div className='card-btns'>
+                        <Button
+                          variant='primary'
+                          onClick={() => onHandleSelect(item.id)}
+                          disabled={!isLoggedIn}
+                        >
+                          Go to details
+                        </Button>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+          
+        ) : (
+          <>
+            <CarouselSlide />
+            <Banner />
+          </>
+        )}
+      </div>
     </>
-
   );
-
 };
+
 
 export default Home;
