b9d0ccdf314ddd4902a3cdf867a21f88590363cd
Daniela Moreno <dani9292@hotmail.com>
2023-08-31 16:42:40 -0500

    Replace url for create reservations

--- src/redux/ReservationCreateSlice.js
+++ src/redux/ReservationCreateSlice.js
@@ -21,7 +21,7 @@ export const createReservation = createAsyncThunk(
     };
 
     const response = await axios.post(
-      `http://127.0.0.1:3001/api/v1/items/${itemId}/reservations`,
+      `https://wanderpalbackend.onrender.com/api/v1/items/${itemId}/reservations`,
       payload
     );
     return response.data;
